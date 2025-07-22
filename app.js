9de39f149595aa47dd1b1918ee6fe71047bda34b
Amit Jirobe <amitjirobe07@gmail.com>
2025-07-22 08:44:07 +0530

    Error Handling

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const Listing = require("./models/listing.js");
 const path=require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync=require("./utils/wrapAsync.js");
+const ExpressError=require("./utils/ExpressError.js");
 
 
 app.set("view engine", "ejs");
@@ -53,11 +55,12 @@ app.get("/listings/:id",async (req,res)=>{
 
 
 //Create Route
-app.post("/listings",async (req,res)=>{
-   const newListing=new Listing(req.body.listing);
-   await newListing.save();
-   res.redirect("/listings");
+app.post("/listings", wrapAsync (async (req,res,next)=>{
+        const newListing=new Listing(req.body.listing);
+        await newListing.save();
+        res.redirect("/listings");
 })
+);
 
 
 //Edit Route
@@ -69,7 +72,7 @@ app.get("/listings/:id/edit",async (req,res) => {
 });
 
 //update Route
-app.put("/Listings/:id",async (req,res) => {
+app.put("/listings/:id",async (req,res) => {
     let { id } =req.params;
     await Listing.findByIdAndUpdate(id ,{ ...req.body.listing});
     res.redirect(`/listings/${id}`);
@@ -98,7 +101,15 @@ app.delete("/listings/:id", async(req,res) =>{
  //  res.send("successful Tsting");
 //});
 
+app.all("*",(req,res,next) =>{
+   next(new ExpressError(404,"Page Not Found!!" ));
+});
 
+app.use((err,req,res,next)=>{
+
+   let {statusCode,message} = err;
+   res.status(statusCode).send(message);
+});
 
 app.listen(8080 ,(req,res)=>{
    console.log(`working : https://localhost:3000`);
